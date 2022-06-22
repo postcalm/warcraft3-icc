1ebf8cf07b164d2fc3bc8204183925eba22bb611
isamposebe <nikita.sokol1999@mail.ru>
2022-06-22 19:17:45 +0300

    Changing the stats of a Priest

--- src/priest/init.lua
+++ src/priest/init.lua
@@ -2,13 +2,17 @@
 function Priest.Init()
     local items_list = {"ARMOR_ITEM", "ATTACK_ITEM", "HP_ITEM"}
     local items_spells_list = {"ARMOR_500", "ATTACK_1500", "HP_90K"}
+
     Priest.hero = Unit(PLAYER_1, PRIEST, Location(4200., 200.), 90.)
 
     EquipSystem.RegisterItems(items_list, items_spells_list)
     EquipSystem.AddItemsToUnit(Priest.hero, items_list)
 
     Priest.hero:SetLevel(80)
-    Priest.hero:SetMana(2000)
+
+    Priest.hero:SetLife(5000);
+    Priest.hero:SetMana(5000);
+
 
     Priest.hero:AddAbilities(FLASH_HEAL, RENEW, CIRCLE_OF_HEALING)
 
