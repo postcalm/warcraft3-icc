c5007b28f3e85b55fa0bea9a6a14875696ccbdfd
postcalm <infinityrestriction@gmail.com>
2023-06-17 18:41:51 +0700

    Добавлены иконки при выборе персонажа

--- libs/HeroSelector/heroSelector.lua
+++ libs/HeroSelector/heroSelector.lua
@@ -47,56 +47,56 @@ end
 function HeroSelector.InitDKSelector()
     HeroSelector.dk = Frame(Frame:GetFrameByName("DeathKnight_Button"))
     HeroSelector.dk:SetTooltip(deathknight_tooltip, deathknight_text)
-    HeroSelector.dk:Disable()
+    HeroSelector.dk:Hide()
     HeroSelector.ConfirmCharacter(HeroSelector.dk)
 end
 
 function HeroSelector.InitDruidSelector()
     HeroSelector.druid = Frame(Frame:GetFrameByName("Druid_Button"))
     HeroSelector.druid:SetTooltip(druid_tooltip, druid_text)
-    HeroSelector.druid:Disable()
+    HeroSelector.druid:Hide()
     HeroSelector.ConfirmCharacter(HeroSelector.druid)
 end
 
 function HeroSelector.InitShamanSelector()
     HeroSelector.shaman = Frame(Frame:GetFrameByName("Shaman_Button"))
     HeroSelector.shaman:SetTooltip(shaman_tooltip, shaman_text)
-    HeroSelector.shaman:Disable()
+    HeroSelector.shaman:Hide()
     HeroSelector.ConfirmCharacter(HeroSelector.shaman)
 end
 
 function HeroSelector.InitWarriorSelector()
     HeroSelector.warrior = Frame(Frame:GetFrameByName("Warrior_Button"))
     HeroSelector.warrior:SetTooltip(warrior_tooltip, warrior_text)
-    HeroSelector.warrior:Disable()
+    HeroSelector.warrior:Hide()
     HeroSelector.ConfirmCharacter(HeroSelector.warrior)
 end
 
 function HeroSelector.InitMageSelector()
     HeroSelector.mage = Frame(Frame:GetFrameByName("Mage_Button"))
     HeroSelector.mage:SetTooltip(mage_tooltip, mage_text)
-    HeroSelector.mage:Disable()
+    HeroSelector.mage:Hide()
     HeroSelector.ConfirmCharacter(HeroSelector.mage)
 end
 
 function HeroSelector.InitRogueSelector()
     HeroSelector.rogue = Frame(Frame:GetFrameByName("Rogue_Button"))
     HeroSelector.rogue:SetTooltip(rogue_tooltip, rogue_text)
-    HeroSelector.rogue:Disable()
+    HeroSelector.rogue:Hide()
     HeroSelector.ConfirmCharacter(HeroSelector.rogue)
 end
 
 function HeroSelector.InitWarlockSelector()
     HeroSelector.warlock = Frame(Frame:GetFrameByName("Warlock_Button"))
     HeroSelector.warlock:SetTooltip(warlock_tooltip, warlock_text)
-    HeroSelector.warlock:Disable()
+    HeroSelector.warlock:Hide()
     HeroSelector.ConfirmCharacter(HeroSelector.warlock)
 end
 
 function HeroSelector.InitHunterSelector()
     HeroSelector.hunter = Frame(Frame:GetFrameByName("Hunter_Button"))
     HeroSelector.hunter:SetTooltip(hunter_tooltip, hunter_text)
-    HeroSelector.hunter:Disable()
+    HeroSelector.hunter:Hide()
     HeroSelector.ConfirmCharacter(HeroSelector.hunter)
 end
 
