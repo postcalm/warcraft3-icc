783b96a46ebb0af9b0cbd396483deb23c3252fa3
postcalm <infinityrestriction@gmail.com>
2025-06-09 14:27:44 +0700

    Use fixed colors

--- src/common/color.lua
+++ src/common/color.lua
@@ -1,22 +1,12 @@
 ---@author meiso
 
--- Формат: transparency-red-green-blue
-function _dec2hex(red, green, blue)
-    red = string.format("%%x", red)
-    green = string.format("%%x", green)
-    blue = string.format("%%x", blue)
-    return "00" .. red .. green .. blue
-end
-
-
 Color = {
-    --- Конвертирует цвет из десятичной в шестнадцатеричную систему
-    dec2hex = _dec2hex,
-    ORANGE = _dec2hex(235, 185, 60),
-    RED = _dec2hex(255, 0, 0),
+    ORANGE = "00ebb93c",
+    RED = "00ff0000",
 }
 
 --- Задать цвет тексту
+---@param text string Текст
 ---@param color Color Цвет
 function set_color(text, color)
     return "|c" .. color .. text .. "|r"
