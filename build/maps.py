004f83037928ae335e1841e578bb73f991bf6d2d
postcalm <infinityrestriction@gmail.com>
2025-06-22 21:23:11 +0700

    Change custom code extension

--- build/maps.py
+++ build/maps.py
@@ -4,18 +4,22 @@ from pathlib import Path
 
 from build.settings import Settings
 
+CUSTOM_CODE_EXT = ".cst"
+MAP_CUSTOM_CODE = f"map{CUSTOM_CODE_EXT}"
+WCT_CUSTOM_CODE = f"wct{CUSTOM_CODE_EXT}"
+
 test_map_settings = Settings(
     map="Test.w3x",
     entry_point="tests/main.lua",
-    map_custom_code=Path("map.cc"),
-    wct_custom_code=Path("wct.cc"),
+    map_custom_code=Path(MAP_CUSTOM_CODE),
+    wct_custom_code=Path(WCT_CUSTOM_CODE),
 )
 
 lower_tier_settings = Settings(
     map="LowerTier.w3x",
     entry_point="src/entry_point/main.lua",
-    map_custom_code=Path("map.cc"),
-    wct_custom_code=Path("wct.cc"),
+    map_custom_code=Path(MAP_CUSTOM_CODE),
+    wct_custom_code=Path(WCT_CUSTOM_CODE),
 )
 
 
